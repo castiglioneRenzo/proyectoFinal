58eac59c14cafbca056ec7ba870cdfac0403e6b8
RENZO AGUSTIN CASTIGLIONE <castiglione.renzo@gmail.com>
2022-08-29 19:49:30 -0300

    cambio en plantillas html, una sola para los forms

--- appMascotas/views.py
+++ appMascotas/views.py
@@ -6,6 +6,8 @@ def inicio(request):
     return render(request, 'landingPage.html')
 
 def mascotaFormulario(request):
+    titulo="AppMascotas - Nueva Mascota"
+    titulo2="Nueva Mascota"
     if request.method == 'POST':
         miFormulario = MascotasFormulario(request.POST)
         print(miFormulario)
@@ -13,7 +15,7 @@ def mascotaFormulario(request):
             info = miFormulario.cleaned_data
             mascota = Mascota(id=info['id'],nombre=info['nombre'],apellido=info['apellido'],raza=info['raza'],descripcion=info['descripcion'],fecha_nacimiento=info['fechaNacimiento'])
             mascota.save()
-            return render(request, 'landingPage.html')
+            return render(request, 'forms.html', {"titulo":titulo, "titulo2":titulo2})
     else:
         miFormulario = MascotasFormulario()
-    return render(request, 'mascotaForm.html', {"miFormulario":miFormulario})
+    return render(request, 'forms.html', {"miFormulario":miFormulario, "titulo":titulo, "titulo2":titulo2})
